fbf8fea19551c3fa5651c4f6eea186f956495663
softwarenacho <trsgarp@gmail.com>
2019-02-26 03:32:27 -0600

    fb script js outside

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
+(() => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then( () => {
+      console.log('sw ok');
+    });
+  }
+});
+
 $("#bio-toggle").click( () => {
   $('#bio-modal').animate({
     height: "toggle",
     opacity: "toggle"
   });
 });
-
-(() => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then( () => {
-    });
-  }
-});
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=400212347411734&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
