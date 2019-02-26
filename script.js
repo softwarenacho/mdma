b3c251985fd713d3bc99413bd2dc620de070540b
softwarenacho <trsgarp@gmail.com>
2019-02-26 03:14:27 -0600

    responsive adjustments

--- script.js
+++ script.js
@@ -5,37 +5,10 @@ $("#bio-toggle").click( () => {
   });
 });
 
-(() => {
-  'use strict';
-
-  let app = {
-    modal: document.getElementById('bio-toggle')
-  };
-
-  let offline = {
-    email: 'sanabria@futuregamemusic.com',
-    bio: `
-      MentalDanceMusicAlgorithm is an alter ego which takes form representing itself in a live act of techno music.\n
-      It arises after eight years of sound experimentation, consuming electronic music every day.\n
-      Developing hypnotic and enveloping sessions as dj, looking for the way and filtering the steps to follow as a music producer.\n
-      Throughout this time he was exploring to arrive at an adequate sound which faithfully represents what the music is for him and to where wants to guide people when he share music with them.\n
-      With 8 years immersed in the music scene, 5 years of music production, after a year just creating sounds within the studio and going deeper into the handling of physical synthesizers that will lead to a more complete project and a more concrete idea, born MentalDanceMusicAlgorithm.\n
-      A live act manipulating synthesizers creating a techno sound with which it seeks to achieve a self-exploration through the physical and mental dance thus achieving a hypnotic and unique process.`
-  }
-
-  app.offline = localStorage.offline;
-  if (app.offline) {
-    app.offline = JSON.parse(app.offline);
-  } else {
-    app.offline = offline;
-  }
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then( () => {
-    });
-  }
-
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then( () => {
+  });
+}
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
