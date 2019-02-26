0b91f7d8dee931c231b7b711c860b09a37c95a24
softwarenacho <trsgarp@gmail.com>
2019-02-26 17:16:29 -0600

    simpler sw runner

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-  console.log('script', navigator)
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then( () => {
-      console.log('sw ok');
-    });
-  }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then( () => {
+  });
+}
