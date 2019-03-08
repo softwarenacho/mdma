f4e15dfa075d14026bb91c156ffe3013aa873c41
softwarenacho <trsgarp@gmail.com>
2019-03-08 04:35:08 -0600

    last video and poster gif fix ever

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-$("#bio-toggle").click( () => {
-  $('#bio-modal').animate({
-    height: "toggle",
-    opacity: "toggle"
+$(() => {
+  let video = document.getElementById('video');
+  if (video.canPlayType('video/mp4').replace('no', '').length > 0) {
+    video.src = 'mdma.mp4';
+  } else {
+    video.poster = 'mdma.gif';
+  }
+  video.style.display = 'block';
+  $("#bio-toggle").click( () => {
+    $('#bio-modal').animate({
+      height: "toggle",
+      opacity: "toggle"
+    });
   });
 });
