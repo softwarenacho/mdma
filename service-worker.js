270e91ed8b2cef6afa641e57db2fe6b68d7d19b0
softwarenacho <trsgarp@gmail.com>
2019-03-08 04:36:02 -0600

    sw back

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
-// const name = 'mdma';
-// const files = ['/index.html', '/styles.css', 'logo/512.png', '/mdma.mp4', '/mdma.gif'];
+const name = 'mdma';
+const files = ['/index.html', '/styles.css', 'logo/512.png', '/mdma.mp4', '/mdma.gif'];
 
-// self.addEventListener('install', e => {
-//   e.waitUntil(
-//     caches.open(name).then( cache => {
-//       return cache.addAll(files);
-//     })
-//   );
-// });
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(name).then( cache => {
+      return cache.addAll(files);
+    })
+  );
+});
 
-// self.addEventListener('activate', e => {
-//   e.waitUntil(
-//     caches.keys().then( keyList => {
-//       return Promise.all( keyList.map( key => {
-//         if (key !== name) {
-//           return caches.delete(key);
-//         }
-//       }));
-//     })
-//   );
-//   return;
-// });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then( keyList => {
+      return Promise.all( keyList.map( key => {
+        if (key !== name) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return;
+});
 
-// self.addEventListener('fetch', e => {
-//   e.respondWith(
-//     caches.match(e.request).then( response => {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then( response => {
+      return response || fetch(e.request);
+    })
+  );
+});
