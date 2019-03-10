97c53c9e8929f1529145fd3aed714c1b2faf1f1b
softwarenacho <trsgarp@gmail.com>
2019-03-10 03:40:29 -0600

    ios working

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 $(() => {
+
   let video = document.getElementById('video');
   if (video.canPlayType('video/mp4').replace('no', '').length > 0) {
     video.src = 'https://res.cloudinary.com/nuxuicourt/video/upload/v1552043413/mdma_nzfo4e.mp4';
   }
   video.style.display = 'block';
-  let clickHandler = ('ontouchstart' in document.documentElement ? 'touchstart' : 'click');
-  $('#bio-toggle').bind(clickHandler, function() {
-    $('#bio-modal').animate({
-      height: "toggle",
-      opacity: "toggle"
-    });
+
+  $('#bio-toggle').click( () => {
+    $('#bio-modal').toggle();
   });
+
 });
