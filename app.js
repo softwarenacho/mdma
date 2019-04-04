db3f4187b1e0d226755e55bdbda9f5e85185d28e
softwarenacho <trsgarp@gmail.com>
2019-04-04 17:16:40 -0600

    bg video changed to image

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(() => {
 
-  let video = document.getElementById('video');
-  if (video.canPlayType('video/mp4').replace('no', '').length > 0) {
-    video.src = 'https://res.cloudinary.com/nuxuicourt/video/upload/v1552043413/mdma_nzfo4e.mp4';
-  }
-  video.style.display = 'block';
+  // let video = document.getElementById('video');
+  // if (video.canPlayType('video/mp4').replace('no', '').length > 0) {
+  //   video.src = 'https://res.cloudinary.com/nuxuicourt/video/upload/v1552043413/mdma_nzfo4e.mp4';
+  // }
+  // video.style.display = 'block';
 
   $('#bio-toggle').click( () => {
     $('#bio-modal').toggle();
